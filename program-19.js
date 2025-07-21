503bab2ee16d734f2a5007ba50564f453771450f
Bansrishukla <bsgeetshukla@gmail.com>
2025-07-21 11:18:48 +0530

    Hello Pro

---
+++ program-19.js
@@ -0,0 +1,13 @@
+// Program to check if a number is positive, negative, or zero
+
+// Input number
+let num = -5;
+
+// Check the number
+if (num > 0) {
+    console.log("Positive");
+} else if (num < 0) {
+    console.log("Negative");
+} else {
+    console.log("Zero");
+}
