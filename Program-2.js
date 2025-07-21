81ea385ffc1db2d041f4366dbbb50aaa498d0646
Bansrishukla <bsgeetshukla@gmail.com>
2025-07-21 10:26:28 +0530

    New Pro

---
+++ Program-2.js
@@ -0,0 +1,19 @@
+
+// 2. Find the Maximum of Two Numbers
+// Definition: Given two numbers, print the larger one.
+
+// Function to find the maximum of two numbers
+function findMax(num1, num2) {
+    if (num1 > num2) {
+        console.log(num1);
+    } else {
+        console.log(num2);
+    }
+}
+
+// Example input
+let num1 = 5;
+let num2 = 9;
+
+// Calling the function
+findMax(num1, num2);
