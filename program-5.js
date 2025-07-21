841e0abec36084633d4bc83e49a85b824dba54dc
Bansrishukla <bsgeetshukla@gmail.com>
2025-07-21 11:10:39 +0530

    Hello Pro

---
+++ program-5.js
@@ -0,0 +1,9 @@
+// Program to print multiplication table of a number
+
+// Input number
+let num = 3;
+
+// Print multiplication table
+for (let i = 1; i <= 10; i++) {
+    console.log(`${num} x ${i} = ${num * i}`);
+}
