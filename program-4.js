218cfdf190d6cbfef07a37b4de470724a30db296
Bansrishukla <bsgeetshukla@gmail.com>
2025-07-21 11:05:55 +0530

    Hello Pro

---
+++ program-4.js
@@ -0,0 +1,13 @@
+// Program to calculate factorial of a number
+
+// Input number
+let num = 4;
+let factorial = 1;
+
+// Calculate factorial using a loop
+for (let i = 1; i <= num; i++) {
+    factorial *= i;
+}
+
+// Output the result
+console.log(factorial);
