7c96d16fbaeac18ddceeeea44ddd4748fb99c85c
Bansrishukla <bsgeetshukla@gmail.com>
2025-07-21 11:16:52 +0530

    Hello Pro

---
+++ program-20.js
@@ -0,0 +1,11 @@
+// Program to find power (a^b)
+
+// Input values
+let a = 2;
+let b = 3;
+
+// Calculate power using Math.pow
+let result = Math.pow(a, b);
+
+// Output the result
+console.log(result);
